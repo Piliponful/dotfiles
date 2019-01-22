86d645477590d1204c8d4c8208bd586bd4bb8d09
Piliponful <piliponful@gmail.com>
2019-01-22 21:01:13 +0200

    remove dotfiles clone command, remove nvm install and source .zshrc from install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,13 @@ sudo apt -y install tmux
 sudo apt -y install zsh
 sudo apt -y install git
 
-git clone https://github.com/Piliponful/dotfiles.git ~/dotfiles
-
 cp ~/dotfiles/.zshrc ~/
 cp ~/dotfiles/.gitconfig ~/
 cp ~/dotfiles/.vimrc ~/
 
 git clone https://github.com/zsh-users/antigen.git ~/antigen
 
+command -v zsh | sudo tee -a /etc/shells
 chsh -s zsh
-source ~/.zshrc
-zsh -i -c "nvm install 10.0.0"
 
 echo 'Execute this command in order to install color themes: bash -c  "$(wget -qO- https://git.io/vQgMr)"'
