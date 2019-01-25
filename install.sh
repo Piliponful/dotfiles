aef92c81350cdd64ff1901bb249696448823de2b
Piliponful <piliponful@gmail.com>
2019-01-26 01:49:30 +0200

    add ycm compilation step

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --ts-completer
 
 chsh -s zsh
 
